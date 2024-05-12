494d50b8a11238718fe8367b5670ae76a20b5140
Tessa Delsener <wildtessa.70@icloud.com>
2024-05-12 16:28:36 -0700

    Initial Commit

---
+++ CameraPractice_submission/CameraPractice_submissionApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraPractice_submissionApp.swift
+//  CameraPractice_submission
+//
+//  Created by Tessa Delsener on 5/12/24.
+//
+
+import SwiftUI
+
+@main
+struct CameraPractice_submissionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CameraPractice_submission/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CameraPractice_submission
+//
+//  Created by Tessa Delsener on 5/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
